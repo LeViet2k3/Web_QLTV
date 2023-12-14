a161e9279a99bb875a187021be587af575d81e94
Le Viet <viet.gm.2k3@gmail.com>
2023-12-14 20:51:52 +0700

    -update UI

--- PHP/admin/statistics.php
+++ PHP/admin/statistics.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
+    <script src="https://cdn.jsdelivr.net/npm/echarts"></script>
     <link rel="stylesheet" href="../../CSS/style.css">
     <link rel="stylesheet" href="../../CSS/statistics.css">
     <title>Statistics</title>
@@ -42,7 +43,7 @@
             include('../libs/helper.php');
             Database::db_connect();
             $sql_select_users = "SELECT Email, UserName, Gender, Place_of_origin, A_phone_number
-        FROM users WHERE Users_status = 'Đang hoạt động' AND Roles = 1";
+                                FROM users WHERE Users_status = 'Đang hoạt động' AND Roles = 1";
             if (Database::db_execute($sql_select_users)) {
                 echo '<table>';
                 echo '<tr>';
@@ -73,7 +74,7 @@
         </div>
         <div class="book_views">
             <h2>Book Views Statistics</h2>
-            <canvas id="myChart1" width="1" height="1"></canvas>
+            <div id="myChart1" style="width: 90vh; height: 100vh;"></div>
         </div>
     </div>
     <div class="statistical">
@@ -136,35 +137,61 @@
                 borderColor.push(randomColor.replace('0.8', '1'));
             }
 
-            var ctx = document.getElementById('myChart1').getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'pie',
-                data: {
-                    labels: labels,
-                    datasets: [{
-                        label: 'Số lượng sách 1',
-                        data: values_sach2,
-                        backgroundColor: backgroundColor,
-                        borderColor: borderColor,
-                        borderWidth: 1
-                    }]
+            var option = {
+                tooltip: {
+                    trigger: 'item',
+                    formatter: '{a} <br/>{b}: {c} ({d}%)',
                 },
-                options: {
-                    plugins: {
-                        legend: {
-                            display: true, // Hiển thị legend
+                legend: {
+                    display: true,
+                    orient: 'vertical',
+                    right: 10,
+                    top: 20,
+                },
+                series: [{
+                    name: 'Nguồn truy cập website',
+                    type: 'pie',
+                    radius: ['30%', '60%'],
+                    avoidLabelOverlap: false,
+                    itemStyle: {
+                        // borderRadius: 10,
+                        borderColor: '#fff',
+                        borderWidth: 3,
+                    },
+                    label: {
+                        show: true,
+                        position: 'inside',
+                        formatter: '{d}%',
+                        fontSize: 12,
+                        fontWeight: 'bold',
+                        fontFamily: "Arial",
+                        color: 'black',
+                    },
+                    emphasis: {
+                        label: {
+                            show: true,
+                            fontSize: '30',
+                            fontWeight: 'bold',
+                            fontFamily: "Arial",
+                        }
+                    },
+                    labelLine: {
+                        show: false,
+                    },
+                    data: values_sach2.map((value, index) => ({
+                        value,
+                        name: labels[index],
+                        itemStyle: {
+                            color: backgroundColor[index],
+                            borderColor: borderColor[index],
+                            borderWidth: 1,
                         },
-                        color: '#fff',
-                        anchor: 'end',
-                        align: 'start',
-                        offset: 10,
-                        font: {
-                            size: '10' // Cỡ chữ của giá trị phần trăm
-                        },
-                        display: true // Đảm bảo plugin datalabels được kích hoạt
-                    }
-                }
-            });
+                    })),
+                }]
+            };
+
+            var myChart = echarts.init(document.getElementById('myChart1'));
+            myChart.setOption(option);
         });
 </script>
 <script src="../../assets/js/main.js"></script>
